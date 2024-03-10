956775d9e050e4e2ff535aecacf729a0c758af25
Chri790 <156339774+Chri790@users.noreply.github.com>
2024-03-10 16:12:42 +0000

    Images

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/Workspace";
+import image from "../images/Workspace.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
